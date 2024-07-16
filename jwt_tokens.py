517fb0b8fb9f40a2a93ca67ee478436355b1f092
Pranav Kolte <pranavkolte111@gmail.com>
2024-07-16 07:27:43 +0530

    minor fix

--- jwt_tokens.py
+++ jwt_tokens.py
@@ -7,40 +7,32 @@ import config
 def create_access_token(payload: dict, expires_delta: _datetime.timedelta) -> str:
     expire: datetime.datetime = _datetime.datetime.utcnow() + expires_delta
     payload.update({"exp": expire})
-    return _jwt.encode(payload=payload,
-                       key=config.SECRET_KEY,
-                       algorithm=config.ALGORITHM)
+    return _jwt.encode(
+        payload=payload, key=config.SECRET_KEY, algorithm=config.ALGORITHM
+    )
 
 
 def is_valid(token: str) -> dict:
     try:
-        payload: dict = _jwt.decode(jwt=token,
-                                    key=config.SECRET_KEY,
-                                    algorithms=[config.ALGORITHM])
+        payload: dict = _jwt.decode(
+            jwt=token, key=config.SECRET_KEY, algorithms=[config.ALGORITHM]
+        )
 
         expiration_time = _datetime.datetime.utcfromtimestamp(payload["exp"])
 
         if not expiration_time > _datetime.datetime.utcnow():
-            return {
-                'status': 'EXPIRED_TOKEN'
-            }
+            return {"status": "EXPIRED_TOKEN"}
 
-        return {
-            'status': 'authorised'
-        }
+        return {"status": "authorised"}
 
     except _jwt.ExpiredSignatureError:
-        return {
-            'status': 'EXPIRED_TOKEN'
-        }
+        return {"status": "EXPIRED_TOKEN"}
 
     except _jwt.InvalidTokenError:
-        return {
-            'status': 'INVALID_TOKEN'
-        }
+        return {"status": "INVALID_TOKEN"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # ----CREATE TOKEN-----------------
     # email: str = "abc@gmail.com"
     # access_token = create_access_token(
@@ -50,6 +42,10 @@ if __name__ == '__main__':
     # print(access_token)
 
     # -------VALIDATE TOKEN------
-    print(is_valid("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-                            ".eyJzdWIiOiJhYmNAZ21haWwuY29tIiwiZXhwIjoxNzIwODA1NDAyfQ"
-                            ".ditAfbEMQlcT9lyfWkFl8ZoeOTWbYwO354FyCgEZRGc"))
+    print(
+        is_valid(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+            ".eyJzdWIiOiJhYmNAZ21haWwuY29tIiwiZXhwIjoxNzIwODA1NDAyfQ"
+            ".ditAfbEMQlcT9lyfWkFl8ZoeOTWbYwO354FyCgEZRGc"
+        )
+    )
