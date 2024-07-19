3c124bdae9e6c0b6f1a5209f6a9719e95fe181fa
Pranav Kolte <pranavkolte111@gmail.com>
2024-07-19 08:39:10 +0530

    fixed refers

--- refers.py
+++ refers.py
@@ -1,37 +1,54 @@
+import fastapi as _fastapi
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
-import fastapi as _fastapi
+import sqlalchemy.orm.query
 
 import config
-import models.user as _user
+from models.user import User
 
 engine = _sql.create_engine(config.DATABASE_URL)
 Session = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_user_info(email: str):
-    db = Session()
-    user = db.query(_user.User).filter(_user.User.email == email).first()
-    if not user:
-        raise _fastapi.HTTPException(status_code=404, detail="Not Found")
-    db.close()
-    return user
+def get_user_info(db: _orm.Session, email: str) -> str:
+    """
+    This Function takes email and returns User info with referral ID
+    :param db: sqlalchemy.orm.Session
+    :param email: str
+    :return: models.user.User
+    """
+    try:
+        user: User = db.query(User).filter(User.email == email).first()
+        if not user:
+            raise _fastapi.HTTPException(status_code=404, detail="Not Found")
+        return user.referral_id
+    finally:
+        db.close()
 
 
-def get_refer_info(email: str):
-    user = get_user_info(email=email)
-    referral_id = user.referral_id
-    clauses = [_user.User.referral_code == referral_id]
-    res = Session().query(_user.User).filter(_sql.or_(*clauses))
-    # .from_statement(_sql.text(f"SELECT id, email, name, time FROM user WHERE referral_code={referral_id};")).all()
-    response = {}
+def get_refer_info(email: str) -> dict:
+    """
+    This Function takes user email and gives referrals of user
+    :param email: str
+    :return: dict
+    """
+    # user: User =
+    referral_id: str = get_user_info(db=Session(), email=email)
+    clauses: list = [User.referral_code == referral_id]
+    res: sqlalchemy.orm.query.Query = Session().query(User).filter(_sql.or_(*clauses))
+    response: dict = {}
     idx = 0
     for ref_user in res.all():
         idx += 1
-        response[idx] = {'id': ref_user.id, 'email': ref_user.email, 'name': ref_user.name, 'time': ref_user.time}
+        response[idx] = {
+            "id": ref_user.id,
+            "email": ref_user.email,
+            "name": ref_user.name,
+            "time": ref_user.time,
+        }
 
     return response
 
 
 if __name__ == "__main__":
-    print(get_refer_info("nitesh342@gmail.com"))
+    print(get_refer_info("abra@gmail.com"))
